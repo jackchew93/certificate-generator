098e795e6dc2bd0220c95945928e5c7277df3824
jackchew93 <110660102+jackchew93@users.noreply.github.com>
2025-11-01 15:10:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def index():
     <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Certificate Generator</title>
+        <title>E-CertiGen</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body class="bg-light">
